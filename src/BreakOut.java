95f34539f66a3d75c125d68a8f5459d4f78212d7
jordantothboi <tothjordan14@gmail.com>
2026-03-04 14:12:59 -0600

    d

--- src/BreakOut.java
+++ src/BreakOut.java
@@ -188,17 +188,26 @@ class BreakOut extends JPanel implements Runnable, KeyListener, MouseListener
             if (!ball.onBoard) {
                 if (diceNum == 6) {
                     if (currentPlayer == 0) ball.setGridPosition(grid, 6, 1);   // blue
-                    if (currentPlayer == 1) ball.setGridPosition(grid, 2, 8);   // green
+                    if (currentPlayer == 1) ball.setGridPosition(grid, 1, 8);   // green
                     if (currentPlayer == 2) ball.setGridPosition(grid, 6, 13);  // red
                     if (currentPlayer == 3) ball.setGridPosition(grid, 8, 13);  // yellow
                 } else {
-                    return;
+                    whoPlays[currentPlayer]=false;
+                    if(currentPlayer==3){
+                        whoPlays[0]=true;
+                    }
+                    else{
+                        whoPlays[currentPlayer+1]=true;
+                    }
+
+
                 }
             } else {
                 for (int i = 0; i < diceNum; i++) {
                     ball.moveOneSquare(grid);
                 }
             }
+
             repaint();
         }
     }
